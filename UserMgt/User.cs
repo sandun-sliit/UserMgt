8502d5d2762a3018178bc00c5545a951fa5d6f0e
Ishini <Ishini@DESKTOP-1P1BJS0>
2023-05-04 12:10:06 +0530

    Add project files.

---
+++ UserMgt/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserMgt
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ UserMgt/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UserMgt
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; 
+        //public byte[] PasswordHash { get; set; }
+        //public byte[] PasswordSalt { get; set; }
+    }
+}
